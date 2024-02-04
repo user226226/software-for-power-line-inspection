ff21510e282e66a67ff84198129db6e004a686c9
Vilena Amanova <79453600+vilenaamanova@users.noreply.github.com>
2024-02-04 22:50:16 +0300

    update

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,6 +13,21 @@ const Map = ({ onMarkerClick }) => {
       { coordinates: [54.919, 52.319], info: 'Информация о точке 1' }
     ];
 
+
+    // // Отображение зоны на карте
+    // const zoneData = {
+    //     name: 'Example Zone',
+    //     coordinates: [
+    //       [54.912, 52.32],
+    //       [54.924, 52.32],
+    //       [54.924, 52.38],
+    //       [54.912, 52.38]
+    //     ]
+    //   };
+
+    // const polygon = DG.polygon(zoneData.coordinates, { color: 'white' }).addTo(map);
+
+
     pointsData.forEach((point) => {
       const marker = DG.marker(point.coordinates);
       marker.on('click', () => {
